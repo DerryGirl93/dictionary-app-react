7858c3e10acd394898f3433dc125d034264d1c93
DerryGirl93 <80162415+DerryGirl93@users.noreply.github.com>
2021-05-15 22:07:28 +0100

    Created Search Form

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-
+import logo from "./logo.png"
 import './App.css';
+import Dictionary from "./Dictionary"
 
-function App() {
+export default function App() {
   return (
+
     <div className="App">
+      <div className="container">
       <header className="App-header">
-        
+        <img src={logo} className="App-logo img-fluid" alt="logo"/>
       </header>
+      <main>
+        <Dictionary/>
+      </main>
+      <footer className="text-center"><a href="https://github.com/DerryGirl93/dictionary-app-react" target="_blank" rel="noreferrer">Open source code</a> by <a href="https://www.linkedin.com/in/emma-o-deorain-2b98408b/" target="_blank" rel="noreferrer">Emma O'Deorain</a></footer>
+      </div>
     </div>
   );
 }
-
-export default App;

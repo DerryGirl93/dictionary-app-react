a17976f5559f76d6b9cb4167024f7f3dcebb2145
DerryGirl93 <80162415+DerryGirl93@users.noreply.github.com>
2021-05-15 22:46:25 +0100

    Added Dictionary API intergration

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,4 +1,5 @@
 import React, {useState} from "react";
+import axios from "axios";
 import "./Dictionary.css"
 
 export default function Dictionary(){
@@ -7,7 +8,14 @@ export default function Dictionary(){
 
     function search(event){
         event.preventDefault();
-        alert(`Searching for ${keyword}`)
+
+        let apiUrl=`https://api.dictionaryapi.dev/api/v2/entries/en_GB/${keyword}`
+        axios.get(apiUrl).then(handleResponse)
+    }
+
+    function handleResponse(response){
+        console.log(response.data[0]);
+
     }
 
     function handleKeywordChange(event){

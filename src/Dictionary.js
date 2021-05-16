dab05ae2431e3d180bcf8acf9ffae2f7d6423559
DerryGirl93 <80162415+DerryGirl93@users.noreply.github.com>
2021-05-16 16:43:38 +0100

    Added Photos API

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,11 +1,13 @@
 import React, {useState} from "react";
 import axios from "axios";
-import "./Dictionary.css"
-import Results from "./Results"
+import "./Dictionary.css";
+import Results from "./Results";
+import Photos from "./Photos";
 
 export default function Dictionary(){
     let [keyword, setKeyword] = useState(null);
     let [results, setResults] = useState(null);
+    let [photos, setPhotos] = useState(null);
 
 
     function search(event){
@@ -13,7 +15,18 @@ export default function Dictionary(){
 
         let apiUrl=`https://api.dictionaryapi.dev/api/v2/entries/en_GB/${keyword}`
         axios.get(apiUrl).then(handleResponse)
+
+        let pexelsApiKey = "563492ad6f9170000100000194fe90ce4fe043a68a1d53df85ce3789"
+        let pexelsUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`
+        let headers = {Authorization: `Bearer ${pexelsApiKey}`};
+         axios.get(pexelsUrl,{headers:headers}).then(handlePexelsResponse)
     }
+
+    function handlePexelsResponse(response){
+        setPhotos(response.data.photos);
+
+    }
+
 
     function handleResponse(response){
         setResults(response.data[0]);
@@ -32,6 +45,7 @@ export default function Dictionary(){
         </form>
 
         <Results results={results}/>
+        <Photos photos ={photos} />
          
     </div>
     );
